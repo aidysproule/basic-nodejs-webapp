9ccb50d936f8100bc1ad14b6b8f90d7d6c39bc03
Adrian Sproule <a_sproule@yahoo.co.uk>
2023-08-23 19:36:16 +0100

    node.js app initial push

---
+++ index.js
@@ -0,0 +1,46 @@
+const http = require('http');
+const randomstring = require("randomstring");
+const random_name = require('node-random-name');
+const slugify = require('slugify');
+
+const express = require('express');
+const app = express();
+
+var httpString;
+
+const server = http.createServer((req,res) => {
+    httpString = randomstring.generate() + "\n";
+    httpString = httpString+ random_name() + "\n";
+    httpString = httpString+ slugify(httpString,"-") + "\n";
+    res.end(httpString);
+});
+
+
+
+server.listen(8000, '0.0.0.0', ()=> {
+    console.log('Listening for requests on port 8000');
+})
+
+app.get('/', function (req, res) {
+    httpString = randomstring.generate() + "\n";
+    httpString = httpString+ random_name() + "\n";
+    httpString = httpString+ slugify(httpString,"-") + "\n";
+    res.end(httpString);
+    httpString=""
+  })
+
+  app.get('/name', function (req, res) {
+    httpString = httpString+ random_name() + "\n";
+    res.end(httpString);
+    httpString=""
+  })
+
+  app.get('/password', function (req, res) {
+    httpString = randomstring.generate() + "\n";
+    res.end(httpString);
+    httpString=""
+  })
+
+  app.listen(3000,() =>{
+    console.log('Express is listening on port 3000');
+  })
