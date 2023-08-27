c1a1ee8e955865be38b9085ed2d0d80d7641d00d
Adrian Sproule <a_sproule@yahoo.co.uk>
2023-08-27 10:27:48 +0100

    Trigger Jenkins

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const server = http.createServer((req,res) => {
     httpString = httpString+ slugify(httpString,"-") + "\n";
     httpString = httpString+ "Hey Dave!"+ "\n";
     httpString = httpString+ "The secret password is: Password1"+ "\n";
+    httpString = httpString+ "Everyone was kung fu fighting!"+ "\n";
     res.end(httpString);
 });
 
@@ -29,6 +30,7 @@ app.get('/', function (req, res) {
     httpString = httpString+ slugify(httpString,"-") + "\n";
     httpString = httpString+ "Hey Bob!"+ "\n";
     httpString = httpString+ "The secret password is actaully just: 1234567890"+ "\n";
+    httpString = httpString+ "Baa baa black sheep"+ "\n";
     res.end(httpString);
     httpString=""
   })
